ad2b77b8dcbef56740cc6a1f854709e7a1af75f3
viktoriaszalkai <viktoriaszalkai04@gmail.com>
2024-11-11 13:56:16 +0100

    class HengerProgram

--- src/main/java/hu/szamalk/HengerProgram.java
+++ src/main/java/hu/szamalk/HengerProgram.java
@@ -4,7 +4,13 @@ import hu.szamalk.model.LyukasHenger;
 import hu.szamalk.model.MertaniHenger;
 import hu.szamalk.model.TomorHenger;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+
+public class HengerProgram {
+
+    private List<MertaniHenger> hengerek;
+
     public static void main(String[] args) {
         MertaniHenger mertani = new MertaniHenger(30,10);
         TomorHenger tomor = new TomorHenger(10,20,2);
@@ -14,4 +20,23 @@ public class Main {
         System.out.println("ly = " +lyukas);
 
         }
+
+
+
+    public HengerProgram() {
+        hengerek = new ArrayList<>();
     }
+    public double AtlagTerfogat(){
+        return 0.0;
+    }
+    public double csovekSulya(){
+        return 0.0;
+    }
+    private List<MertaniHenger> lista(){
+
+        return hengerek;
+    }
+    public void run(){
+
+    }
+}
